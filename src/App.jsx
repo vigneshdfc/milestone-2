ee16fad22bbe5000a7eb2132ad831f12d6b146dd
Radha02062001 <rajalakshmi96462@gmail.com>
2022-11-02 11:44:26 +0530

    miles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Client from "./component/reviews/slide";
 import Bottom from "./component/footer/footer";
 import Partner from "./component/reviewpartner/review";
-import Front from "../../WEB/Book/src/Components/Header";
+import Front from "./component/header/header";
 
 const App = () => {
   return (
