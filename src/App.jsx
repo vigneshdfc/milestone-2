ce9ef67df2e979979389c2684dd3e19e6730db71
ramabhi2001 <abirami@gmail.com>
2022-11-02 11:20:05 +0530

    miles

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Client from "./component/reviews/slide";
 import Bottom from "./component/footer/footer";
 import Partner from "./component/reviewpartner/review";
-import Front from "../../WEB/Book/src/Components/Header";
+// import Front from "../../WEB/Book/src/Components/Header";
+import Front from "./component/header/header";
+import SimpleSlider from "./component/heroslider/hero";
 
 const App = () => {
   return (
     <>
       <Front />
-      <Client />;
-      <Bottom />;
+      <SimpleSlider />
+      <Client />
+      <Bottom />
     </>
   );
 };
