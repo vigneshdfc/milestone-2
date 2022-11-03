e34f016fbf636f713b5c51130c9239f7bc8c0aec
vigneshdfc <vigneshdfc02@gmail.com>
2022-11-03 15:11:16 +0530

    feat(review)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Client from "./component/reviews/slide";
 import Bottom from "./component/footer/footer";
-import Partner from "./component/reviewpartner/review";
 import Front from "./component/header/header";
-import  SimpleSlider from "./component/heroslider/hero";
+import SimpleSlider from "./component/heroslider/hero";
 import Images from "./component/cardshover/cards";
+
 const App = () => {
   return (
     <>
       <Front />
-      <SimpleSlider/>
-      <Images/>
+      <SimpleSlider />
+      <Images />
       <Client />;
       <Bottom />;
     </>
