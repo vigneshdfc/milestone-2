a2fc00b98b3918c44b525a17f8087d2755204746
28112001 <aaronstains2001@gmail.com>
2022-11-04 11:25:51 +0530

    fix:dropdown header

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Front = () => {
-  const [dropdown, setDropdown] = React.useState(false);
   return (
     <div className="mainpage">
-      {" "}
       <div className="hero">
         <h2>
           Government Of India
@@ -16,28 +13,10 @@ const Front = () => {
       </div>
       <div className="Topnav">
         <h3>LOGO</h3>
-        <Link to="/home">Home</Link>
-        <div className="Dropdown">
-          <div
-            className="dropbtn_content"
-            onMouseEnter={() => setDropdown(true)}
-            onMouseLeave={() => setDropdown(false)}
-          >
-            <button class="dropbtn">
-              Book
-              <i class="fa fa-caret-down"></i>
-            </button>
-            {dropdown && (
-              <div className="list">
-                <a href="#Book">
-                  <h6> Book an Appointments</h6>
-                </a>
-              </div>
-            )}
-          </div>
-        </div>
-        <Link to="/Service">Our Services</Link>
-        <Link to="/About">ABOUT US</Link>
+        <a href="#">Home</a>
+        <a href="#">Book an Appointment</a>
+        <a href="#">Our Services</a>
+        <a href="#">About Us</a>
         <div className="but">
           <button>LOGIN</button>
         </div>
@@ -46,3 +25,24 @@ const Front = () => {
   );
 };
 export default Front;
+{
+  /* <div className="Dropdown">
+          <div
+            className="dropbtn_content"
+            onMouseEnter={() => setDropdown(true)}
+            onMouseLeave={() => setDropdown(false)}
+          >
+            <div class="dropbtn">
+              Book
+              <i class="fa fa-caret-down"></i>
+            </div>
+            {dropdown && (
+              <div className="list">
+                <a href="#Book">
+                  <h6> Book an Appointments</h6>
+                </a>
+              </div>
+            )}
+          </div>
+            </div>*/
+}
